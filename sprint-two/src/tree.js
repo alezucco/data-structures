54c3c3de861ccbaac922c43e25361d853d010593
alezucco <alejandrozuccolillo@hotmail.com>
2014-09-11 16:40:59 -0700

    tree structure complete

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,9 @@
 var makeTree = function(value){
   var newTree = {};
+  _.extend(newTree, treeMethods)
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+
   return newTree;
 };
 
@@ -12,13 +14,38 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value){
 
+  return this.children.push(makeTree(value));
+
+
 };
 
 treeMethods.contains = function(target){
+  var result=false;
+  var search=function(array){
+    for (var i=0;i<array.length;i++){
 
+      if(array[i].value===target){
+        console.log(array[i].value);
+        result=true;
+      }else if(array[i].children.length>0){
+
+        //if(array[i].children===undefined){debugger;}
+        search(array[i].children);
+      }
+    }
+  }
+  search(this.children);
+  return result;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//[{value:value}, {1value:value}].push(object-->value:value)
+//[[ ][ ]]
+//tree.value == 7;
+//trre.children[0] = {value:value}
+//tree.children[0][value] -- > value
+//tree.children[0][this.value] -- > undefined
+//tree.children[0][7] -- > undefined
