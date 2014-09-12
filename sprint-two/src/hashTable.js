7c1dec0522c42f323e72df5d7302b88f84b1edad
alezucco <alejandrozuccolillo@hotmail.com>
2014-09-12 15:18:32 -0700

    hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,42 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+
+  var temp=[];
+  //temp.push(k);
+  //temp.push(v);
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(i)){
+    temp.push(this._storage.get(i));
+    temp.push([k,v]);
+    this._storage.set(i,temp);
+  }else{
+    this._storage.set(i, [k,v]);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
-};
+  if (this._storage.get(i)){
+    if(this._storage.get(i)[0]===k){
+      return this._storage.get(i)[1];
+    }
+    if (Array.isArray(this._storage.get(i)[0])){
+      for (var j=0;j<this._storage.get(i).length;j++){
+        if(this._storage.get(i)[j][0]===k){
+          return this._storage.get(i)[j][1];
+        }
+      }
+    }
+  }else{
+    return null;
+  }
+}
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,null);
 };
 
 
@@ -21,3 +47,7 @@ HashTable.prototype.remove = function(k){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+//each on ._storage >>storage
+//[[ ],[ ],[ ]]
